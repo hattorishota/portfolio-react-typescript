ba5aa66f0e46a1972ccf5698adc271060f4b6d3b
hattorishota <hattorishota.eng@gmail.com>
2022-04-23 18:26:46 +0900

    no message

---
+++ src/component/App.js
@@ -0,0 +1,23 @@
+import logo from '../logo.svg';
+import '../sass/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>check</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
